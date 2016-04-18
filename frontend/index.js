fb03a32fd3646a8181b3cc3294babf15f33de533
thondery <thondery@163.com>
2016-04-18 23:46:23 +0800

    更新小屏幕弹出菜单功能

--- frontend/index.js
+++ frontend/index.js
@@ -4,4 +4,13 @@ $(() => {
   // tooltip
   $('[data-toggle="tooltip"]').tooltip()
 
+  $('[data-menubar="menu"], #tools-bar>.mask').on('click', e => {
+    if ($('#tools-bar').hasClass('online')) {
+      $('#tools-bar').removeClass('online')
+    }
+    else {
+      $('#tools-bar').addClass('online')
+    }
+  })
+
 })
